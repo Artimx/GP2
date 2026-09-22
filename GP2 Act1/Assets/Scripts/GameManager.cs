38f18d6077ee00bc6283d7d12798f2fb4e05299b
ArtiPri <mathewsapon@gmail.com>
2026-09-22 11:07:06 +0800

    Smthn

---
+++ GP2 Act1/Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager
+    Instance
+    { get; private set; }
+
+    public int score = 0;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void AddScore(int value)
+    {
+    score += value;
+        Debug.Log("Score: " + score);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
